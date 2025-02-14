631bda73982fcf2833ad2e1a9a57deb90086a239
Bowon Ryu <bowon.ryu@samsung.com>
2025-02-14 14:52:15 +0900

    add ime fullscreen mode test
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

---
+++ fullscreen_ime_tpk/nuitext.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace NUIText
+{
+    class Program : NUIApplication
+    {
+        const string TAG = "NUIText";
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            GenerateUI(new Size(480, 780));
+        }
+
+        public void GenerateUI(Size windowSize)
+        {
+            Window window = Window.Instance;
+            //window.WindowSize = windowSize;
+
+            var view = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    LinearAlignment = LinearLayout.Alignment.Begin,
+                    CellPadding = new Size2D(10, 10),
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                BackgroundColor = Color.Black,
+            };
+            window.Add(view);
+
+            var field = NewTextField("");
+            field.PlaceholderText = "Single line";
+            field.GetInputMethodContext().FullScreenMode = true;
+            view.Add(field);
+
+            var editor = NewTextEditor("");
+            editor.PlaceholderText = "Multi line";
+            editor.GetInputMethodContext().FullScreenMode = true;
+            view.Add(editor);
+
+            var button = NewButton("FullScreenMode:True");
+            view.Add(button);
+            button.Clicked += (s, e) =>
+            {
+                field.GetInputMethodContext().FullScreenMode = !field.GetInputMethodContext().FullScreenMode;
+                editor.GetInputMethodContext().FullScreenMode = !editor.GetInputMethodContext().FullScreenMode;
+
+                button.Text = "FullScreenMode:" + field.GetInputMethodContext().FullScreenMode;
+            };
+        }
+
+        public TextField NewTextField(string text)
+        {
+            var field = new TextField
+            {
+                Text = text,
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 100,
+                PixelSize = 30.0f,
+                BackgroundColor = Color.White,
+                MaxLength = 200,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            return field;
+        }
+
+        public TextEditor NewTextEditor(string text)
+        {
+            var editor = new TextEditor
+            {
+                Text = text,
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 300,
+                PixelSize = 30.0f,
+                BackgroundColor = Color.White,
+            };
+            return editor;
+        }
+
+        public Button NewButton(string text)
+        {
+            var button = new Button(NewButtonStyle())
+            {
+                Text = text,
+                PointSize = 20.0f,
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                ItemHorizontalAlignment = HorizontalAlignment.Center,
+            };
+            return button;
+        }
+
+        public ButtonStyle NewButtonStyle()
+        {
+            var style = new ButtonStyle
+            {
+                CornerRadius = 0.0f,
+                BackgroundColor = new Selector<Color>()
+                {
+                    Normal = new Color(0.25f, 0.75f, 1.0f, 1.0f),
+                    Pressed = new Color(0.25f, 0.75f, 1.0f, 0.3f),
+                },
+                Overlay = new ImageViewStyle()
+                {
+                    BackgroundColor = new Selector<Color>()
+                    {
+                        Pressed = new Color(0, 0, 0, 0.1f),
+                        Other = new Color(1, 1, 1, 0.1f),
+                    },
+                },
+                Text = new TextLabelStyle()
+                {
+                    TextColor = new Color(0.0f, 0.0f, 0.0f, 1.0f),
+                }
+            };
+            return style;
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
